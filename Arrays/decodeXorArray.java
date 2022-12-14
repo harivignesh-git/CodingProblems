71fc1064372c88610446b4798b385f17bc0e8bb8
harivignesh-git <120357983+harivignesh-git@users.noreply.github.com>
2022-12-14 21:45:04 +0530

    Create decodeXorArray.java

---
+++ Arrays/decodeXorArray.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] decode(int[] encoded, int first) {
+        int size = encoded.length;
+        int[] res = new int[size+1];
+        res[0] = first;
+        for(int i=0; i<size; i++) {
+            res[i+1] = res[i] ^ encoded[i];
+        }
+        return res;
+    }
+}
