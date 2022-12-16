2e99e928eaaa704d86ba8801571c25819a377850
HARIVIGNESH S <120357983+harivignesh-git@users.noreply.github.com>
2022-12-16 17:57:00 +0530

    Create setMatrixZeroes.java

---
+++ Arrays/setMatrixZeroes.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int row=matrix.length,col=matrix[0].length;
+        int[] dummy1=new int[row];
+        int[] dummy2=new int[col];
+        Arrays.fill(dummy1,-1);
+        Arrays.fill(dummy2,-1);
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(matrix[i][j]==0){
+                    dummy1[i]=0;
+                    dummy2[j]=0;
+                }
+            }
+        }
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(dummy1[i]==0 || dummy2[j]==0){
+                    matrix[i][j]=0;
+                }
+            }
+        }
+    }
+}
