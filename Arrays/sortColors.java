bb96a30679e036275b77ccb0fe03fcaad0e48992
HARIVIGNESH S <120357983+harivignesh-git@users.noreply.github.com>
2022-12-16 19:14:33 +0530

    Create sortColors.java

---
+++ Arrays/sortColors.java
@@ -0,0 +1,32 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int n=nums.length;
+        int temp;
+        int low=0;
+        int mid=0;
+        int high=n-1;
+        while(mid<=high){
+            switch(nums[mid]){
+                case 0:{
+                    temp=nums[low];
+                    nums[low]=nums[mid];
+                    nums[mid]=temp;
+                    low++;
+                    mid++;
+                    break;
+                }
+                case 1:{
+                    mid++;
+                    break;
+                }
+                case 2:{
+                    temp=nums[mid];
+                    nums[mid]=nums[high];
+                    nums[high]=temp;
+                    high--;
+                    break;
+                }
+            }
+        }
+    }
+}
